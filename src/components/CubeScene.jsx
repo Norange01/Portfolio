90010db76af8ba6b892218d0e7e02824b163453b
Norange01 <alrajabn@mcmaster.ca>
2025-05-22 18:34:25 -0400

    Hero now resizes with window

--- src/components/CubeScene.jsx
+++ src/components/CubeScene.jsx
@@ -6,7 +6,7 @@ function Box() {
   return (
     <mesh rotation={[0.5, 0.5, 0]}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="#59c9a5" />
+      <meshStandardMaterial color="#f2bac9" />
     </mesh>
   );
 }
